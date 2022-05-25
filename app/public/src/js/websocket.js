bff829344f504bb613ac1fa395098421cf6c4dca
Fernando Sigali <fernando.sigali@isisim.org.br>
2022-05-24 23:10:38 -0300

    change hosts

--- app/public/src/js/websocket.js
+++ app/public/src/js/websocket.js
@@ -2,7 +2,8 @@ import io from 'socket.io-client';
 
 const wsPort = 3001;
 
-export const socket = io.connect(`http://10.167.1.223:${wsPort}`, {
+// export const socket = io.connect(`http://10.167.1.223:${wsPort}`, {
+export const socket = io.connect(`http://192.168.15.7:${wsPort}`, {
     withCredentials: true,
     transports : ['websocket'] 
 });
