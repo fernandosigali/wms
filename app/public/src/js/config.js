67b526883cb9b392634d1d4d01df463338f6b741
fernandosigali <fernando.sigali@isisim.org.br>
2022-05-25 11:19:59 -0300

    redo config.js

---
+++ app/public/src/js/config.js
@@ -0,0 +1,12 @@
+export const env = {
+    rtls: {
+        host: '10.167.1.25',
+        port: 8000,
+        endpoint: 'wms',
+        token: '12345', 
+    },
+    wms: {
+        host: '10.167.1.223',
+        port: 3001,
+    }
+}
